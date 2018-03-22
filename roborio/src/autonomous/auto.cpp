450376656838dbf77193171403ba8b27a3760ca8
Wilson Watson <wwatso1tech@gmail.com>
2018-03-22 08:40:44 -0500

    Fixed autonomous speeds

--- roborio/src/autonomous/auto.cpp
+++ roborio/src/autonomous/auto.cpp
@@ -7,21 +7,14 @@
 #define SLIPPING_MULTIPLIER .8
 //#define SLIPPING_MULTIPLIER .67
 
-
 // All auto calls use these variables, so I condensed them into a single define
 #define ALL_THE_VARS robot, drive, left, right, axle_width, WHEEL_CONSTANT, CURVE_P, intake_f, lift_f
 
-#define MAX_VELOCITY .65
 #define MIN_VELOCITY .4
 
 #define OUTTAKE_V -.25
 #define LIFT_START_V 0.3
 #define LIFT_V 0.8
-/*
- #define OUTTAKE_V -0.4
- #define LIFT_START_V 0.2
- #define LIFT_V 0.4
- */
 
 #include "autotools.h"
 
@@ -29,9 +22,9 @@
  *  AUTO MODE DEFINITIONS
  */
 
-AUTO_FUNC(Straight, (), (), 0, 100);
+AUTO_FUNC(Straight, .45, (), (), 0, 100);
 
-AUTO_FUNC(P2toSwL,
+AUTO_FUNC(P2toSwL, .45,
 		(std::thread t([&]() { frc::Timer timer; SmartDashboard::PutNumber("Auto Seq",0);timer.Start(); while(timer.Get() < 2.5) {
 
 		if(timer.Get() > 1.5){ lift_f(false, LIFT_V); } else { lift_f(false, LIFT_START_V); }
@@ -44,49 +37,49 @@ AUTO_FUNC(P2toSwL,
 		0, 25);
 
 
-AUTO_FUNC(P2toSwR7, (), (intake_f(OUTTAKE_V,0.8);),
+AUTO_FUNC(P2toSwR7,.45,  (), (intake_f(OUTTAKE_V,0.8);),
 		0, 60);
 
-AUTO_FUNC(P2toSwR6, (), (P2toSwR7(ALL_THE_VARS);),
+AUTO_FUNC(P2toSwR6, .45, (), (P2toSwR7(ALL_THE_VARS);),
 		-10, -20);
 
-AUTO_FUNC(P2toSwR5, (std::thread t([&]() { frc::Timer timer; SmartDashboard::PutNumber("Auto Seq",0);timer.Start(); while(timer.Get() < 1.5) { lift_f(true, LIFT_V); } lift_f(true, 0.0); SmartDashboard::PutNumber("Auto Seq",1); });), (t.join();P2toSwR6(ALL_THE_VARS);),
+AUTO_FUNC(P2toSwR5, .45, (std::thread t([&]() { frc::Timer timer; SmartDashboard::PutNumber("Auto Seq",0);timer.Start(); while(timer.Get() < 1.5) { lift_f(true, LIFT_V); } lift_f(true, 0.0); SmartDashboard::PutNumber("Auto Seq",1); });), (t.join();P2toSwR6(ALL_THE_VARS);),
 		0, -20);
 
-AUTO_FUNC(P2toSwR4, (), (drive.set(-.2, -.2); intake_f(-3.0*OUTTAKE_V,0.8); intake_f(-3.0*OUTTAKE_V,0.8); P2toSwR5(ALL_THE_VARS);),
+AUTO_FUNC(P2toSwR4, .45, (), (drive.set(-.2, -.2); intake_f(-3.0*OUTTAKE_V,0.8); intake_f(-3.0*OUTTAKE_V,0.8); P2toSwR5(ALL_THE_VARS);),
 		-20, 40);
 
-AUTO_FUNC(P2toSwR2,
+AUTO_FUNC(P2toSwR2,.45,
 		(std::thread t([&]() { frc::Timer timer; SmartDashboard::PutNumber("Auto Seq",0);timer.Start(); while(timer.Get() < 1.0) { lift_f(true, -LIFT_V); } lift_f(true, 0.0); SmartDashboard::PutNumber("Auto Seq",1); });),
 		(t.join();Wait(.2);P2toSwR4(ALL_THE_VARS);),
 		0, -65);
 
-AUTO_FUNC(P2toSwR,
+AUTO_FUNC(P2toSwR,.45,
 		(std::thread t([&]() { frc::Timer timer; SmartDashboard::PutNumber("Auto Seq",0);timer.Start(); while(timer.Get() < 2.5) { if(timer.Get() > 1.5){ lift_f(false, LIFT_V); } else { lift_f(false, LIFT_START_V); } } lift_f(false, 0.0); SmartDashboard::PutNumber("Auto Seq",1); });),
 		(t.join(); drive.set(-.25, -.25); Wait(0.3); intake_f(OUTTAKE_V,0.8); drive.set(0, 0); P2toSwR2(ALL_THE_VARS);),
 		0, 65);
 
-AUTO_FUNC(P3toSwR2, (), (intake_f(OUTTAKE_V,0.8);),
+AUTO_FUNC(P3toSwR2,.45,  (), (intake_f(OUTTAKE_V,0.8);),
 		-15, 15)
 
-AUTO_FUNC(P3toSwR,
+AUTO_FUNC(P3toSwR,.45,
 		(std::thread t([&]() { frc::Timer timer; SmartDashboard::PutNumber("Auto Seq",0);timer.Start(); while(timer.Get() < 2.5) { if(timer.Get() > 1.5){ if(lift_f(false, LIFT_V)) break; } else { lift_f(false, LIFT_START_V); } } lift_f(false, 0.0); SmartDashboard::PutNumber("Auto Seq",1); });),
 		(t.join(); Wait(.3); P3toSwR2(ALL_THE_VARS);),
 		0, 120);
 
-AUTO_FUNC(P3toScR4,
+AUTO_FUNC(P3toScR4,.65,
 		(drive.set(-.3, -.3); intake_f(-3.0*OUTTAKE_V,0.4); drive.set(0, 0);std::thread t([&]() { frc::Timer timer; SmartDashboard::PutNumber("Auto Seq",0);timer.Start(); while(true) { if(lift_f(true, LIFT_V)) break; } lift_f(false, 0.0); SmartDashboard::PutNumber("Auto Seq",1); });),
 		(t.join(); intake_f(2.25*OUTTAKE_V, 0.8);),
 		-50, -30,
 		9, 45);
 
-AUTO_FUNC(P3toScR2,
+AUTO_FUNC(P3toScR2,.65,
 		(std::thread t([&]() { frc::Timer timer; SmartDashboard::PutNumber("Auto Seq",0);timer.Start(); while(timer.Get() < 3.0) {  lift_f(true, -LIFT_V); } lift_f(true, 0.0); SmartDashboard::PutNumber("Auto Seq",1); });),
 		(t.join();Wait(.2); drive.set(-.2, -.2); intake_f(-3.0*OUTTAKE_V,0.4); drive.set(0, 0); P3toScR4(ALL_THE_VARS);),
 		20, -45,
 		-50, 30);
 
-AUTO_FUNC(P3toScR,
+AUTO_FUNC(P3toScR,.65,
 		(std::thread t([&]() { frc::Timer timer; SmartDashboard::PutNumber("Auto Seq",0);timer.Start(); while(true) { if(timer.Get() > 1.5){ if(lift_f(true, LIFT_V)) break; } else { lift_f(false, LIFT_START_V); } } lift_f(false, 0.0); SmartDashboard::PutNumber("Auto Seq",1); });),
 		(t.join(); drive.set(-.3, -.45); Wait(.4); drive.set(0, 0);intake_f(1.25*OUTTAKE_V, 0.8); P3toScR2(ALL_THE_VARS);),
 		10, 140,
--- roborio/src/autonomous/autotools.h
+++ roborio/src/autonomous/autotools.h
@@ -5,7 +5,7 @@
 			&& !drivetrain::driveto(drive, PRIMITIVE_CAT(dc, i), \
 					fabs(left.GetRaw() / WHEEL_CONSTANT),\
 					fabs(right.GetRaw() / WHEEL_CONSTANT), curve_p = CURVE_P,\
-					max_velocity = -MAX_VELOCITY * (v2 > 0 ? 1 : -1), min_velocity = -MIN_VELOCITY * (v2 > 0 ? 1 : -1))) {\
+					max_velocity = -k * (v2 > 0 ? 1 : -1), min_velocity = -MIN_VELOCITY * (v2 > 0 ? 1 : -1))) {\
 		SmartDashboard::PutNumber("left", left.GetRaw() / WHEEL_CONSTANT);\
 		SmartDashboard::PutNumber("right", right.GetRaw() / WHEEL_CONSTANT);\
 	}\
@@ -15,10 +15,10 @@
 	Wait(0.5);\
 	if(v1 != v2){left.Reset(); right.Reset();}
 
-#define AUTO_FUNC(name, start, end, ...) void name(frc::RobotBase *robot, drivetrain::differential_drive& drive,\
+#define AUTO_FUNC(name, speed, start, end, ...) void name(frc::RobotBase *robot, drivetrain::differential_drive& drive,\
 		Encoder& left, Encoder& right, double axle_width, double WHEEL_CONSTANT,\
 		double CURVE_P, void (*intake_f)(double, double), bool (*lift_f)(bool, double)) {EXPAND start;\
 left.Reset();\
 right.Reset();\
-VA_ITER_2(__AUTO_FUNC, (), __VA_ARGS__);VA_ITER_2(_AUTO_FUNC, (), __VA_ARGS__); EXPAND end}
+VA_ITER_2(__AUTO_FUNC, (), __VA_ARGS__);VA_ITER_2(_AUTO_FUNC, speed, __VA_ARGS__); EXPAND end}
 
